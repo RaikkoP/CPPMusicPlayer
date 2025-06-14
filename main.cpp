1ca915e07c5a269fda9c653678cf3d79ad962478
Raikko <raikko.prants@gmail.com>
2025-06-15 01:09:20 +0300

    messing around trying to figure c++ out, created seperate functions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <vector>
 #include <filesystem>
 
+
+// Loop through files and find all the audio files
+int loopThroughPath(const char* input) {
+    for (const auto & file : std::filesystem::directory_iterator(input)) {
+        std::cout << file.path() << std::endl;
+    }
+    return 0;
+}
+
 int main() {
-    // Ask for directory from user
     char input[100];
     std::cout << "What directory would you like to scan for files from?" << std::endl;
     std::cin >> input;
-    // loop through path
-    for (const auto & file : std::filesystem::directory_iterator(input)) {
-         std::cout << file.path() << std::endl;
-    }
+    loopThroughPath(input);
     return 0;
 }
