1b454887df6040041dacd38312d51f821aad1021
Raikko <raikko.prants@gmail.com>
2025-06-15 01:38:47 +0300

    figured out how to convert and made a file extenstion checker using string conversion

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <filesystem>
+#include <string>
 
 
 // Loop through files and find all the audio files
-int loopThroughPath(const char* input) {
+
+//TODO: Might want to find a system that doesn't do this with string but with actual file types
+int loopThroughPath(std::string input) {
     for (const auto & file : std::filesystem::directory_iterator(input)) {
-        std::cout << file.path() << std::endl;
+        std::string converted = (std::string)file.path();
+        unsigned size = converted.size();
+        converted.erase(converted.begin(), converted.end() - 4);
+        if(converted == ".cpp") {
+            std::cout << converted << std::endl;
+        }
     }
     return 0;
 }
 
 int main() {
-    char input[100];
+    std::string input;
     std::cout << "What directory would you like to scan for files from?" << std::endl;
     std::cin >> input;
     loopThroughPath(input);
